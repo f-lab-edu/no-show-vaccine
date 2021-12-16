8ef34a7e95072fdc13fae063d9c77c4b5470bebc
jinwoong <jinwoongma@gmail.com>
2021-12-17 01:45:32 +0900

    Add Institution, Location domain

---
+++ api/src/main/java/com/flab/nsv/domain/institution/Location.java
@@ -0,0 +1,14 @@
+package com.flab.nsv.domain.location;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+	private double longitude;
+
+	private double latitude;
+}
---
+++ api/src/main/java/com/flab/nsv/mapper/InstitutionMapper.java
@@ -0,0 +1,4 @@
+package com.flab.nsv.mapper;
+
+public interface InstitutionMapper {
+}
