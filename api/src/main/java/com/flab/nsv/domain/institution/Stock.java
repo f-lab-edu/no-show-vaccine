1506c40e22a3c2d5fa92705df971005aa29fa25c
oleveloper <oleveloper@gmail.com>
2021-12-17 20:26:28 +0900

    Add Stock entity

---
+++ api/src/main/java/com/flab/nsv/domain/institution/Stock.java
@@ -0,0 +1,19 @@
+package com.flab.nsv.domain.institution;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Stock {
+	private int quantity;
+	private LocalDateTime lastUpdate;
+}
