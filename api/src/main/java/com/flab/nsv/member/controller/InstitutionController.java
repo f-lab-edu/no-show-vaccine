998c2c315972530365007f37566c2b446c4b4f8e
oleveloper <oleveloper@gmail.com>
2021-12-27 14:20:44 +0900

    Change PostMapping to PutMapping
     - updateInstitution method of InstitutionController

--- api/src/main/java/com/flab/nsv/member/controller/InstitutionController.java
+++ api/src/main/java/com/flab/nsv/member/controller/InstitutionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,7 +37,7 @@ public class InstitutionController {
 		return institutionService.getById(id);
 	}
 
-	@PostMapping("/{id}")
+	@PutMapping("/{id}")
 	public void updateInstitution(
 		@PathVariable("id") final long id,
 		@Valid @RequestBody final UpdateInstitutionRequestDto updateInstituionRequestDto) {
